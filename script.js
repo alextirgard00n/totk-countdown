85e91c6c588ac9edd50c648eff219986d07f082c
alextirgard00n <tirgardoon@gmail.com>
2023-03-23 22:26:23 -1000

    added animation to the changing elements

--- script.js
+++ script.js
@@ -6,26 +6,39 @@ const secondsEl = document.querySelector(".seconds");
 
 let countDownDate = new Date("May 12, 2023").getTime();
 
-// Update the count down every 1 second
 function countdown() {
-
-    // Get today's date and time
     let now = new Date().getTime();
-
-    // Find the distance between now and the count down date
     let distance = countDownDate - now;
-
-    // Time calculations for days, hours, minutes and seconds
     let days = Math.floor(distance / (1000 * 60 * 60 * 24));
     let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    daysEl.innerText = days;
-    hoursEl.innerText = hours;
-    minsEl.innerText = minutes;
-    secondsEl.innerText = seconds;
-};
+    if (days != daysEl.innerText) {
+        daysEl.innerText = days;
+        animateCSS(daysEl);
+    }
+
+    if (hours != hoursEl.innerText) {
+        hoursEl.innerText = hours;
+        animateCSS(hoursEl);
+    }
+
+    if (minutes != minsEl.innerText) {
+        minsEl.innerText = minutes;
+        animateCSS(minsEl);
+    }
+
+    if (seconds != secondsEl.innerText) {
+        secondsEl.innerText = seconds;
+        animateCSS(secondsEl);
+    }
+}
+
+function animateCSS(element) {
+    element.classList.add("animation");
+    setTimeout(() => element.classList.remove("animation"), 500);
+}
 
 countdown();
 setInterval(countdown, 1000);
